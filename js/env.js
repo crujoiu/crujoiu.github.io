372c5beccf89f954ab7044b8174718c021920eeb
crujoiu <cristi99k@yahoo.com>
2017-05-12 16:33:00 +0300

    split
    trying to split the code

---
+++ js/env.js
@@ -0,0 +1,8 @@
+function Environment(img, x, y) {
+	"use strict";
+    
+}
+
+window.addEventListener('load', Environment);
+ 
+  
---
+++ js/score.js
@@ -0,0 +1,23 @@
+function Score(x, y) {
+    "use strict";
+    this.x = x;
+    this.y = y;
+    this.drawScore = function () {
+      ctx.font = "bold 14px sans-serif";
+      ctx.fillStyle = "black";
+      ctx.fillText("SCORE: " + score, x, y);
+    };
+    this.updateScore = function () {
+      for (var i = 0; i < gameObstacles.length; i += 1) {
+        var d = sprite.x - (gameObstacles[i].x + gameObstacles[i].w);
+        if (d >= 0 && d < 1) {
+          score += 1;
+        }
+      }
+    };
+    this.resetScore = function () {
+      score = 0;
+    };
+  }
+  
+window.addEventListener('load', Score);
